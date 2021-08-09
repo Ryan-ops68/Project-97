ac040583d6e99ba7c301f9027f7791c24e92203b
Ryan-ops68 <72042413+Ryan-ops68@users.noreply.github.com>
2021-08-09 11:40:50 -0400

    Add files via upload

---
+++ Activity.py
@@ -0,0 +1,7 @@
+limit = int(input("Enter a random number"))
+if (limit>5):
+    print("Not quite")
+elif (limit>9):
+    print("Congratulation you are the Winner!")
+else:
+    print("You have to do better")
