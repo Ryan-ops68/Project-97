5bd6dd4292c9a289a9188b0bc54085024bcc6332
Ryan-ops68 <72042413+Ryan-ops68@users.noreply.github.com>
2021-08-12 10:09:39 -0400

    Update Activity.py

--- Activity.py
+++ Activity.py
@@ -1,7 +1,14 @@
-limit = int(input("Enter a random number"))
-if (limit>5):
-    print("Not quite")
-elif (limit>9):
-    print("Congratulation you are the Winner!")
-else:
-    print("You have to do better")
+import random
+number = random.randint(1,9)
+chances = 0
+while chances<5:
+    limit = int(input("Enter a guess number"))
+    if (limit==number):
+        print("Well done")
+    elif (limit>number):
+        print("Your guess was too high")
+    else:
+        print("You guess was too low")
+    chances+=1
+if not chances<5:
+    print("You lose number was ", number)
